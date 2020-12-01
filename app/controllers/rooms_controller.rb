2095dfe3cf105bb6e7b704abe46f8655b5304db6
satorimod <74944697+satorimod@users.noreply.github.com>
2020-12-01 17:54:41 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,8 @@
 class RoomsController < ApplicationController
+
+  def index
+  end  
+
   def new
     @room = Room.new
   end
@@ -9,15 +13,12 @@ class RoomsController < ApplicationController
       redirect_to root_path
     else
       render :new
-    end    
-  end  
+    end
+  end
 
   private
 
   def room_params
     params.require(:room).permit(:name, user_ids: [])
-  end  
-
-  
-
+  end
 end
